8ee0fa4541ca4a039fb1fb0e7ac8a79cd6ee7579
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-09-18 14:51:45 +0200

    Add project files.

---
+++ TCPConcurrentServer1/EchoServices.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPConcurrentServer1
+{
+
+    public class EchoServices
+    {
+        private  TcpClient _serverSocket;
+        private TcpListener _welcomingSocket;
+        private FileStream _fileStream;
+        String uri = @"C:/temp"; // Root path C://temp/index.html
+        public EchoServices(TcpClient serverSocket , TcpListener welcomingSocket)
+        {
+            _serverSocket = serverSocket;
+            _welcomingSocket = welcomingSocket;
+        }
+        public void DoIt()
+        {
+            using (Stream ns = _serverSocket.GetStream())
+            {
+                StreamWriter sw = new StreamWriter(new BufferedStream(ns));
+                sw.AutoFlush = true;
+                StreamReader sr = new StreamReader(ns);
+
+                // Read data from Client
+                var request = sr.ReadLine();
+                // Check message is not null 
+                while (request != null || request != "")
+                {
+                    // Get /index.html HTTP1.1
+                    // [0] Get
+                    //  [1]/index.html
+                    // [2] HTTP1.1
+                    var brokenMessages = request.Split(' ');
+                    if (brokenMessages.Length != 0)
+                    {
+                        // check first Message
+                        // Get
+                        var firstMessage = brokenMessages[0];
+                        // calls when client send STOP message 
+                        if (firstMessage.Equals("stop"))
+                        {
+                            Console.WriteLine("its mean you want to stop the server");
+                            while (_welcomingSocket.Pending())
+                            {
+                                Thread.Sleep(100);
+                            }
+                            Console.WriteLine("Finally Server is Stopped");
+                            _welcomingSocket.Stop();                           
+                            break;
+                        }
+                        // calls when client send GET /index.html HTTP1.1
+                        else if (firstMessage.Equals("GET") && brokenMessages.Length == 3)
+                        {
+                            string fileName = brokenMessages[1];
+                            string protocol = brokenMessages[2];
+                            // HTTP response header 200-> http response status
+                            sw.Write("HTTP/1.1 200 OK\r\n");
+                            sw.Write("Content-Type: text/html\r\n");
+                            sw.Write("Connection: close\r\n");
+                            sw.Write("\r\n"); //Only to Browser marks data are coming
+                            sw.Write("Hello client\r\n");
+                            sw.Write("Requested file: " + fileName + "\r\n");
+                             // C://temp/index.html
+                            uri = uri + fileName;
+                            // path of resource name
+                            _fileStream = new FileStream(uri, FileMode.Open, FileAccess.Read);
+                            StreamReader fileReader = new StreamReader(_fileStream);
+                            while (!fileReader.EndOfStream)
+                            {
+                                string s = fileReader.ReadLine();
+                                Console.WriteLine(s);
+                                sw.Write(s + "\r\n");
+                            }
+                            sw.Write("\r\n");
+                            sw.Flush();
+                            sw.BaseStream.Flush();
+                            //sw.Close();
+                        }
+                        // calls this when client send general message 
+                        else
+                        {
+                            Console.WriteLine("Client message:" + request);
+                            var respond = request.ToLower();
+                            sw.WriteLine(respond);
+                        }
+                        // server is ready to listen again
+                        request = sr.ReadLine();
+                    }
+                }
+               
+            }
+            
+        }
+
+    }
+}
---
+++ TCPConcurrentServer1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPConcurrentServer1
+{
+    class Program
+    {
+        private static TcpListener _welcomingSocket;
+        private static TcpClient _serverSocket;
+        static void Main(string[] args)
+        {
+            try
+            {
+                var ip = IPAddress.Parse("127.0.0.1");
+                // you have to reserved the server port
+                // You always define port number service here in server 
+                _welcomingSocket = new TcpListener(ip, 6789);
+                Console.WriteLine("Server is ready to listen incoming client request");
+                _welcomingSocket.Start();
+                using (_serverSocket = _welcomingSocket.AcceptTcpClient())
+                {
+                    Console.WriteLine("Client IP" + ((IPEndPoint)_serverSocket.Client.RemoteEndPoint).Address);
+                    EchoServices service = new EchoServices(_serverSocket, _welcomingSocket);
+                    // method is now a multi threaded 
+                    // server recieved multiple message from different clients at the same same time
+
+                    while (true)
+                    {
+                        Task.Run(() => service.DoIt());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                _welcomingSocket.Stop();
+            }
+
+        }
+    }
+}
